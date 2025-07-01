13559b910677987c43c52bca72a2ffad1d27fead
cirogrego <cirogrego@hotmail.com>
2025-07-01 15:35:25 -0300

    First commit

--- cypress/tests/e2e/login.spec.js
+++ cypress/tests/e2e/login.spec.js
@@ -1,11 +1,11 @@
 describe('Real World app', () => {
   it('Login - Success', () => {
     cy.visit('http://localhost:3000/signin')
-    cy.get('#username').type('Dina20')
-    cy.get('#password').type('s3cret')
+    cy.get('#username').type('cirogrego')
+    cy.get('#password').type('123456')
     cy.get('[data-test="signin-submit"]').click()
-  });
-});
+  })
+})
 
  it('Login - Fail', () => {
     cy.visit('http://localhost:3000/signin')
@@ -13,4 +13,29 @@ describe('Real World app', () => {
     cy.get('#password').type('Test')
     cy.get('[data-test="signin-submit"]').click()
     cy.get('[role="alert"]')
-  });
+  })
+
+  it('Login - New User', () => {
+    cy.visit('http://localhost:3000/signin')
+    cy.get('[href="/signup"]').click()
+    cy.get('#firstName').type('Ciro')
+    cy.get('#lastName').type('Dutra')
+    cy.get('#username').type('cirogrego')
+    cy.get('#password').type('123456')
+    cy.get('#confirmPassword').type('123456')
+    cy.get('[type="submit"]').click()
+
+})
+
+it.only('Login - New User Fail', () => {
+    cy.visit('http://localhost:3000/signin')
+    cy.get('[href="/signup"]').click()
+    cy.get('#firstName').type('Donald')
+    cy.get('#lastName').type('Trump')
+    cy.get('#username').type('DonaldTrump')
+    cy.get('#password').type('123456')
+    cy.get('#confirmPassword').type('12345')
+    cy.get('#confirmPassword-helper-text').contains('Password does not match')
+    
+
+})
