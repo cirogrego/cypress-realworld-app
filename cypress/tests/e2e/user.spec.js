7a131309793c20ecf6f69779b6dbe4ab8a1b66cb
cirogrego <cirogrego@hotmail.com>
2025-07-03 12:00:20 -0300

    Userdata added

--- cypress/tests/e2e/user.spec.js
+++ cypress/tests/e2e/user.spec.js
@@ -1,4 +1,4 @@
-import userData from '../e2e/userData.json'
+import userData from '../../fixtures/userData.json'
 
 describe('Real World app', () => {
 
@@ -13,15 +13,27 @@ describe('Real World app', () => {
     confirmpasswordField: "#confirmPassword",
     signupButton:'[type="submit"]',
     confirmpasswordFieldError:"#confirmPassword-helper-text",
-
+    newtransactionButton:'[href="/transaction/new"]',
+    avatarButton:'[src="https://avatars.dicebear.com/api/human/uBmeaz5pX.svg"]',
+    amountField:"#amount",
+    addnoteField:"#transaction-create-description-input",
+    payButton:'[data-test="transaction-create-submit-payment"]',
+    
   }
 
-  it('Login - Success', () => {
+
+  it.only('Login - Success', () => {
     cy.visit('http://localhost:3000/signin')
     cy.get(selectorList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorList.rememberField).click()
     cy.get(selectorList.loginButton).click()
+    cy.get(selectorList.newtransactionButton).click()
+    cy.get(selectorList.avatarButton).click()
+    cy.get(selectorList.amountField).type(100)
+    cy.get(selectorList.addnoteField).type('Test')
+    cy.get(selectorList.payButton).click()
+    cy.get('.MuiAlert-message')
    })
 
  it('Login - Fail', () => {
